0d341dee910a6967ecd18867c2ae39b790372f36
xiaochaochao <xiaochaochao>
2023-04-23 00:54:46 +0800

    test

---
+++ vuepress.config.js
@@ -0,0 +1,22 @@
+import { defineUserConfig } from 'vuepress';
+import { defaultTheme } from 'vuepress'
+import { searchPlugin } from '@vuepress/plugin-search'
+
+export default {
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'External', link: 'https://google.com' },
+      ]
+  }),
+  plugins: [
+    searchPlugin({
+        // 配置项
+      }),
+  ]
+}
